e1106292d744a45d10aee29d3da512319ee64d60
깃ChertHyeon <r4560798@naver.com>
2023-09-02 17:22:20 +0900

    응답 규격화 RsData 도입

--- src/main/java/com/example/restapipractice/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/example/restapipractice/boundedContext/member/controller/MemberController.java
@@ -7,13 +7,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.restapipractice.base.rsData.RsData;
 import com.example.restapipractice.boundedContext.member.entity.Member;
 import com.example.restapipractice.boundedContext.member.service.MemberService;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RestController // 모든 메서드에서 @ResponseBody 생략 가능
@@ -28,21 +31,25 @@ public class MemberController {
 		@NotBlank
 		private String password;
 	}
+	
+	@AllArgsConstructor
+	@Getter
+	public static class LoginResponse {
+		private final String accessToken;
+	}
 	@PostMapping("/login")
 	// @RequestBody : 요청의 본문(Json, Xml 등)을 Java 객체로 변환
-	public String login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
+	public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
 		String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
 
 		resp.addHeader("Authentication", accessToken);
 
-		return """
-                {
-                  "resultCode": "S-1",
-                  "msg": "엑세스 토큰이 생성되었습니다.",
-                  "data": {
-                    "accessToken": "%s"
-                  }
-                }
-                """.formatted(accessToken).stripIndent();
+		// 응답을 규격화한 객체 반환
+		// 잭슨이 Json형태로 규격화하여 반환됨
+		return RsData.of(
+			"S-1",
+			"엑세스토큰이 생성되었습니다.",
+			new LoginResponse(accessToken)
+		);
 	}
 }
