163ced38b324c26ad9d62b93fe7acf8464103c4f
깃ChertHyeon <r4560798@naver.com>
2023-09-02 14:07:28 +0900

    yml 세팅

--- src/test/java/com/example/restapipractice/RestApiPracticeApplicationTests.java
+++ src/test/java/com/example/restapipractice/RestApiPracticeApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.restapipractice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RestApiPracticeApplicationTests {
 
 	@Test
