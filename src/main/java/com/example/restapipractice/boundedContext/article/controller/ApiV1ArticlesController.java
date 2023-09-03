86b58d4cdbdda24e790879ad9ca2fbabca42411c
깃ChertHyeon <r4560798@naver.com>
2023-09-03 11:00:52 +0900

    게시글 수정

--- src/main/java/com/example/restapipractice/boundedContext/article/controller/ApiV1ArticlesController.java
+++ src/main/java/com/example/restapipractice/boundedContext/article/controller/ApiV1ArticlesController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -96,7 +97,7 @@ public class ApiV1ArticlesController {
 		private final Article article;
 	}
 
-	@PostMapping(value = "")
+	@PostMapping(value = "", consumes = APPLICATION_JSON_VALUE)
 	@Operation(summary = "등록", security = @SecurityRequirement(name = "bearerAuth"))
 	public RsData<WriteResponse> write(
 		@AuthenticationPrincipal User user,
@@ -111,6 +112,50 @@ public class ApiV1ArticlesController {
 			writeRs.getResultCode(),
 			writeRs.getMsg(),
 			new WriteResponse(writeRs.getData())
+		);
+	}
+
+	@Data
+	public static class ModifyRequest {
+		@NotBlank
+		private String subject;
+		@NotBlank
+		private String content;
+	}
+
+	@AllArgsConstructor
+	@Getter
+	public static class ModifyResponse {
+		private final Article article;
+	}
+
+	@PatchMapping(value = "/{id}", consumes = APPLICATION_JSON_VALUE)
+	@Operation(summary = "수정", security = @SecurityRequirement(name = "bearerAuth"))
+	public RsData<ModifyResponse> modify(
+		@AuthenticationPrincipal User user,
+		@Valid @RequestBody ModifyRequest modifyRequest,
+		@PathVariable Long id
+	) {
+		Member member = memberService.findByUsername(user.getUsername()).orElseThrow();
+
+		Optional<Article> opArticle = articleService.findById(id);
+
+		if (opArticle.isEmpty()) return RsData.of(
+			"F-1",
+			"%d번 게시물은 존재하지 않습니다.".formatted(id),
+			null
+		);
+
+		RsData canModifyRs = articleService.canModify(member, opArticle.get());
+
+		if (canModifyRs.isFail()) return canModifyRs;
+
+		RsData<Article> modifyRs = articleService.modify(opArticle.get(), modifyRequest.getSubject(), modifyRequest.getContent());
+
+		return RsData.of(
+			modifyRs.getResultCode(),
+			modifyRs.getMsg(),
+			new ModifyResponse(modifyRs.getData())
 		);
 	}
 }
