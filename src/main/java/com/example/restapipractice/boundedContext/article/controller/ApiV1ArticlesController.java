dac4dc091224600adec863a9896ad8e6e541a308
깃ChertHyeon <r4560798@naver.com>
2023-09-03 10:50:59 +0900

    게시물 작성

--- src/main/java/com/example/restapipractice/boundedContext/article/controller/ApiV1ArticlesController.java
+++ src/main/java/com/example/restapipractice/boundedContext/article/controller/ApiV1ArticlesController.java
@@ -5,18 +5,28 @@ import static org.springframework.http.MediaType.*;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.restapipractice.base.rsData.RsData;
 import com.example.restapipractice.boundedContext.article.entity.Article;
 import com.example.restapipractice.boundedContext.article.service.ArticleService;
+import com.example.restapipractice.boundedContext.member.entity.Member;
+import com.example.restapipractice.boundedContext.member.service.MemberService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -26,6 +36,7 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "ApiV1ArticlesController", description = "게시물 CRUD 컨트롤러")
 public class ApiV1ArticlesController {
 	private final ArticleService articleService;
+	private final MemberService memberService;
 
 	@AllArgsConstructor
 	@Getter
@@ -68,6 +79,38 @@ public class ApiV1ArticlesController {
 			"S-1",
 			"성공",
 			new ArticleResponse(article.get())
+		);
+	}
+
+	@Data
+	public static class WriteRequest {
+		@NotBlank
+		private String subject;
+		@NotBlank
+		private String content;
+	}
+
+	@AllArgsConstructor
+	@Getter
+	public static class WriteResponse {
+		private final Article article;
+	}
+
+	@PostMapping(value = "")
+	@Operation(summary = "등록", security = @SecurityRequirement(name = "bearerAuth"))
+	public RsData<WriteResponse> write(
+		@AuthenticationPrincipal User user,
+		@Valid @RequestBody WriteRequest writeRequest
+	) {
+		Member member = memberService.findByUsername(user.getUsername()).orElseThrow();
+		RsData<Article> writeRs = articleService.write(member, writeRequest.getSubject(), writeRequest.getContent());
+
+		if (writeRs.isFail()) return (RsData) writeRs;
+
+		return RsData.of(
+			writeRs.getResultCode(),
+			writeRs.getMsg(),
+			new WriteResponse(writeRs.getData())
 		);
 	}
 }
